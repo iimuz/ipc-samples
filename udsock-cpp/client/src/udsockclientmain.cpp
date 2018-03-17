94f28521fdc48d870e3cea0fac7b1af12fa9387f
iimuz <d.is.iimuz@gmail.com>
2018-03-17 05:57:33 +0000

    feat: initialize unix domain socket server and client programs

---
+++ udsock-cpp/client/src/udsockclientmain.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+/// @brief Unix domain socket を利用した通信クライアントのエントリポイント
+int main()
+{
+  std::cout << "start unix domain socket client.\n";
+
+  std::cout << "end unix domain socket client.\n";
+  return EXIT_SUCCESS;
+}
---
+++ udsock-cpp/server/src/udsockservermain.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+/// @brief Unix domain socket を利用した通信サーバのエントリポイント
+int main()
+{
+  std::cout << "start unix domain socket server.\n";
+
+  std::cout << "end unix domain socket server.\n";
+  return EXIT_SUCCESS;
+}
